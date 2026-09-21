e5d4dce990e861f2807d45e430866dc005a67f46
Ayesha Rafique Bhatti <ayesharafiquebhatti08@gmail.com>
2026-09-21 16:50:06 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+print("===================================")
+print("       TECH PATH EXPLORER")
+print("AI-powered technology career exploration")
+print("===================================")
+
+print()
+print("Let's explore which technology paths")
+print("might be interesting for you!")
